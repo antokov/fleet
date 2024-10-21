1879bbc8c499562c14f0fb7a5051b6b27841ed1d
Toni <anton.kovacevic87@gmail.com>
2024-10-21 12:05:20 +0200

    fix app.run()

--- app.py
+++ app.py
@@ -81,5 +81,4 @@ def rent_details(car_id):
     return "Car not found", 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
